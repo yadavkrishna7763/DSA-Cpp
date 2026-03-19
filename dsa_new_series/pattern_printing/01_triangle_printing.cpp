810346c612071e2a9d54fb6e7930d60925644173
KRISHNA YADAV <krishnayadav7763@KRISHNAs-MacBook-Air-2.local>
2026-03-19 16:41:04 +0530

    printing pattern

---
+++ dsa_new_series/pattern_printing/01_triangle_printing.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+void print1(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0;j<n; j++)
+        {
+            cout<<"*";
+
+        }
+        cout<< endl;
+    }
+}
+void print2(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0;j<=i; j++)
+        {
+            cout<<"* ";
+
+        }
+        cout<< endl;
+    }
+}
+void print3(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0;j<=i; j++)
+        {
+            cout<< j+1;
+
+        }
+        cout<< endl;
+    }
+}
+void print4(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0;j<=i; j++)
+        {
+            cout<< i+1;
+
+        }
+        cout<< endl;
+    }
+}
+void print5(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=n-1;j>=i; j--)
+        {
+            cout<<"* ";
+
+        }
+        cout<< endl;
+    }
+}
+void print6(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=1;j<n-i+1; j++)
+        {
+            cout<<j<<" ";
+            
+
+        }
+        cout<< endl;
+    }
+}
+void print7(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=n-1;j>=i; j--)
+        {
+            cout<<j+1;
+
+        }
+        cout<< endl;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"enter n"<< endl;
+    cin>>n;
+    print6(n);
+   
+}
