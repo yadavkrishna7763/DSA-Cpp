47bea1ba8af35e98ac3e4f60107f834455a06331
Krishna Yadav <krishnayadav7763@gmail.com>
2026-04-01 13:20:39 +0530

    update old all file

--- dsa_new_series/pattern_printing/pattern_printing.cpp
+++ dsa_new_series/pattern_printing/pattern_printing.cpp
@@ -3,94 +3,96 @@ using namespace std;
 
 void print1(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n; j++)
+        for (int j = 0; j < n; j++)
         {
-            cout<<"*";
-
+            cout << "*";
         }
-        cout<< endl;
+        cout << endl;
     }
 }
 void print2(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<=i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            cout<<"* ";
-
+            cout << "* ";
         }
-        cout<< endl;
+        cout << endl;
     }
 }
 void print3(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<=i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            cout<< j+1;
-
+            cout << j + 1;
         }
-        cout<< endl;
+        cout << endl;
     }
 }
 void print4(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<=i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            cout<< i+1;
-
+            cout << i + 1;
         }
-        cout<< endl;
+        cout << endl;
     }
 }
 void print5(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=n-1;j>=i; j--)
+        for (int j = n - 1; j >= i; j--)
         {
-            cout<<"* ";
-
+            cout << "* ";
         }
-        cout<< endl;
+        cout << endl;
     }
 }
 void print6(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=1;j<n-i+1; j++)
+        for (int j = 1; j < n - i + 1; j++)
         {
-            cout<<j<<" ";
-            
-
+            cout << j << " ";
         }
-        cout<< endl;
+        cout << endl;
     }
 }
 void print7(int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=n-1;j>=i; j--)
-        {
-            cout<<j+1;
 
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
         }
-        cout<< endl;
+    
+        for (int j = 0; j <2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+    
+        for (int j = 0; j <n - 1 - 1; j++)
+        {
+            cout << " ";
+        }
+        cout<<endl;
     }
 }
 int main()
 {
     int n;
-    cout<<"enter n"<< endl;
-    cin>>n;
-    print6(n);
-   
+    cout << "enter n" << endl;
+    cin >> n;
+    print7(n);
 }
